e9f66cc1be8c1ef17df9dc16fdde948f1ae529ec
sprack <thomas.price15@imperial.ac.uk>
2017-11-08 17:41:12 +0000

    test for comparable

--- tests/sim/RequestEventTest.java
+++ tests/sim/RequestEventTest.java
@@ -2,6 +2,7 @@ package sim;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RequestEventTest {
@@ -18,5 +19,17 @@ class RequestEventTest {
         }
 
         assertTrue(Math.abs(sum/numSamples - 1) < 0.05);
+    }
+
+    @Test
+    void compareTimes() {
+        RequestEvent e1 = new RequestEvent(1, 0);
+        RequestEvent e2 = new RequestEvent(1, 0);
+        RequestEvent e3 = new RequestEvent(1, 1);
+
+        assertTrue(e1.compareTo(e2) == 0);
+        assertTrue(e1.compareTo(e3) < 0);
+        assertTrue(e3.compareTo(e1) > 0);
+
     }
 }
