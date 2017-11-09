ce10d1e980163023b262f748cf0d99d981aee9b4
Shiraz Butt <shiraz.butt15@imperial.ac.uk>
2017-11-09 00:12:38 +0000

    Moved one for loop for adding to event queue and cache that checked if should be added to cache on all iterations to two separate for loops, the first of which adds to queue and the cache, the second only to the queue.

--- src/sim/CacheSimulation.java
+++ src/sim/CacheSimulation.java
@@ -1,7 +1,7 @@
 package sim;
 
-import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class CacheSimulation {
@@ -10,23 +10,30 @@ public class CacheSimulation {
     private Cache cache;
     private PriorityQueue<RequestEvent> eventQueue = new PriorityQueue<>();
 
-    public CacheSimulation(int cacheSize, int storageSize, EvictionPolicy policy, double simTime) {
-
+    public CacheSimulation(final int cacheSize, final int storageSize, EvictionPolicy policy, final double simTime) {
         this.simTime = simTime;
 
-        Collection<RequestEvent> toCache = new LinkedList<>();
+        // List of events to be cached. These are the first `cacheSize` events.
+        List<RequestEvent> toCache = new LinkedList<>();
 
-        for (int i = 0; i < storageSize; i++) {
+        // First `cacheSize` events are added to queue and inserted into cache, in order 1..`cacheSize`.
+        for (int i = 0; i < cacheSize; i++) {
             RequestEvent e = new RequestEvent(i + 1);
             eventQueue.add(e);
-            if (i < cacheSize) {
-                toCache.add(e);
-            }
+
+            // Adds to end of linked list in O(1) time.
+            toCache.add(e);
+        }
+
+        // Remaining events are added to even queue, but not the cache.
+        for (int i = cacheSize; i < storageSize; i++) {
+            RequestEvent e = new RequestEvent(i + 1);
+            eventQueue.add(e);
         }
 
 
+        // Construct cache with eviction policy described by `policy`.
         switch (policy) {
-
             case FIFO:
                 this.cache = new FIFOCache(toCache);
                 break;
