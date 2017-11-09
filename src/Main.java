e4dbeaa09d0f20882ec8efeefa9226a0766e3fdd
Shiraz Butt <shiraz.butt15@imperial.ac.uk>
2017-11-09 01:27:14 +0000

    Refactor to use Results inner class that is returned from simulate(), rather than printing results in simulate()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import sim.CacheSimulation;
+import sim.CacheSimulation.Results;
 import sim.cache.EvictionPolicy;
 
 public class Main {
     public static void main(String[] args) {
-        CacheSimulation fifoSim = new CacheSimulation(10, 1000, EvictionPolicy.FIFO, 10000);
-        fifoSim.simulate();
+        CacheSimulation fifoSim = new CacheSimulation(10, 1000,
+                EvictionPolicy.FIFO, 50000);
+        Results results = fifoSim.simulate();
+        System.out.println(results);
     }
 }
--- src/sim/CacheSimulation.java
+++ src/sim/CacheSimulation.java
@@ -13,6 +13,7 @@ public class CacheSimulation {
     private double simTime;
     private Cache cache;
     private PriorityQueue<RequestEvent> eventQueue = new PriorityQueue<>();
+    private Results results;
 
     /**
      * Constructs a simulation of a cache that measures the hit ratio and the
@@ -28,6 +29,7 @@ public class CacheSimulation {
                            EvictionPolicy policy, final double simTime) {
 
         this.simTime = simTime;
+        this.results = new Results(storageSize);
 
         // List of events to be cached. These are the first `cacheSize` events.
         List<RequestEvent> toCache = new LinkedList<>();
@@ -56,9 +58,9 @@ public class CacheSimulation {
     /**
      * Performs the simulation, measuring hit ratio and miss rate.
      */
-    public void simulate() {
-        double hits = 0;
-        double total = 0;
+    public Results simulate() {
+        int hits = 0;
+        int total = 0;
 
         RequestEvent e = eventQueue.poll();
         double now = 0;
@@ -81,17 +83,78 @@ public class CacheSimulation {
             e = eventQueue.poll();
         }
 
-        System.out.println("hits = " + hits);
-        System.out.println("total = " + total);
-        System.out.println("hit ratio = " + (hits / total));
-        System.out.println("miss rate = " + (total - hits) / now);
+        return results.calculate(hits, total, now);
+    }
 
-        double sum = 0;
-        for (int i = 0; i < 1000; i++) {
-            sum += 1 / (i + 1.0);
+    // Literally made this an inner class to see if I can still remember how
+    // to use inner classes...
+    public class Results {
+        private int numItems;
+
+        private double hitRatio;
+        private double missRateByMissThroughput;
+        private double missRateByHitRatioAndRates;
+
+        public double getHitRatio() {
+            return hitRatio;
         }
 
-        System.out.println("miss rate 2 = " + (1 - (hits / total)) * sum);
+        public double getMissRateByMissThroughput() {
+            return missRateByMissThroughput;
+        }
+
+        public double getMissRateByHitRatioAndRates() {
+            return missRateByHitRatioAndRates;
+        }
+
+        @Override
+        public String toString() {
+            return "{\n" +
+                    "    Hit ratio: " + this.hitRatio + ",\n" +
+                    "    Miss rate by miss throughput: " + this.missRateByMissThroughput + ",\n" +
+                    "    Miss rate by hit ratio and sum of all rate parameters: " + this.missRateByHitRatioAndRates + ",\n" +
+                    "}";
+        }
+
+        /**
+         * Constructs an object containing the results of the cache
+         * simulation for `numItems` items. These results are:
+         * `hitRatio`: The hit ratio, H / T.
+         * `missRateByMissThroughput`: The miss rate calculated by M / T.
+         * `missRateByHitRatioAndRates`: The miss rate calculate by (1 -
+         * hitRatio) * (sum of rate parameter of each stored item).
+         *
+         * The fields are not safe to access until after `calculate()` has
+         * been called.
+         * @param numItems The number of items in the simulation. This is
+         *                 used to calculate `missRateByHitRatioAndRates`.
+         */
+        private Results(int numItems) {
+            this.numItems = numItems;
+        }
+
+        /**
+         * Calculates the results and sets their fields. Only after this call
+         * can the fields by safely accessed.
+         * @param hits number of hits observed.
+         * @param totalReqs totalReqs number of requests observed.
+         */
+        private Results calculate(int hits, int totalReqs, double totalTime) {
+            this.hitRatio = (double) hits / (double) totalReqs;
+            this.missRateByMissThroughput = ((double) (totalReqs - hits)) / totalTime;
+
+            double sumOfRates = 0;
+
+            // Since for item k, 1 <= k <= numItems, their rate parameters for
+            // inter-request time are 1 / k.
+            for (int i = 0; i < numItems; i++) {
+                sumOfRates += 1 / ((double) i + 1);
+            }
+
+            this.missRateByHitRatioAndRates = (1 - this.hitRatio) * sumOfRates;
+
+            return this;
+        }
     }
 
 }
