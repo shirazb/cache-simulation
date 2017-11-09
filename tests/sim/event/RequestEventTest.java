74b607cb2e1f56dd56e5bc83434a9a1820b2032c
Shiraz Butt <shiraz.butt15@imperial.ac.uk>
2017-11-09 01:30:26 +0000

    Make RequestEventTest clearer

--- tests/sim/event/RequestEventTest.java
+++ tests/sim/event/RequestEventTest.java
@@ -7,18 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RequestEventTest {
 
+    /**
+     * Test soundness of inter arrival distribution by testing the mean of
+     * many results is close to the expected mean.
+     */
     @Test
     void interArrivalTimeIsRealistic() {
-        RequestEvent event = new RequestEvent(4);
-
+        int lambda = 4;
         int numSamples = 100000;
+
+        RequestEvent event = new RequestEvent(lambda);
 
         for (int i = 0; i < numSamples; i++) {
             event.scheduleNextRequest();
         }
 
-//        System.out.println(event.getTime()/numSamples);
-        assertTrue(Math.abs(event.getTime()/numSamples - 4) < 0.05);
+        assertTrue(Math.abs(event.getTime() / numSamples - 4) < 0.05);
     }
 
 }
