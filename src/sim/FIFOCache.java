e9fee205e342e43a28e1611998041dc7bba01cf2
Shiraz Butt <shiraz.butt15@imperial.ac.uk>
2017-11-09 00:52:52 +0000

    Cleaned up and documented code for FIFOCache

--- src/sim/FIFOCache.java
+++ src/sim/FIFOCache.java
@@ -13,13 +13,27 @@ public class FIFOCache implements Cache {
         this.cache = new ArrayList<>(initialCache);
     }
 
+    /**
+     * Simulates the lookup of `e.itemNo` in the cache. If there is a cache
+     * miss, evicts an element according to a FIFO policy and places `e` in
+     * the cache.
+     * @param e Request element to lookup in the cache.
+     * @return `lookUp(e)` <-> `e` was in the cache.
+     */
     @Override
     public boolean fetch(RequestEvent e) {
         boolean cacheHit = cache.contains(e);
+
+        // Implements FIFO by keeping track of the index of the last added
+        // element, and filling up the cache from index 0 upwards. Thus, to
+        // remove an event from the cache and insert e, simply set the
+        // event at that index to be `e`.
         if (!cacheHit) {
             cache.set(indexToEvict, e);
             indexToEvict = (indexToEvict + 1) % cache.size();
         }
+
         return cacheHit;
     }
+
 }
