ca517d7be72c8c5924a1a95524aab4bfc8a5b359
sprack <thomas.price15@imperial.ac.uk>
2017-11-10 18:07:48 +0000

    calculation of standard deviation for interval estimates

--- src/SimulationAverage.java
+++ src/SimulationAverage.java
@@ -5,21 +5,24 @@ import sim.cache.FIFOCache;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.ToDoubleFunction;
 
 public class SimulationAverage {
+
+
+    private static final int NUM_SAMPLES = 12;
+    private static final int SIM_TIME = 10000;
+
     public static void main(String[] args) throws Exception {
 
-        final int simTime = 10000;
-        final int numSamples = 50;
         final int M = 100;
         final int N = 1000;
-
 
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
         List<Future<SimResults>> futureResults = new LinkedList<>();
 
-        for (int i = 0; i < numSamples; i++) {
-            CacheSimulation sim = new CacheSimulation(M, N, new FIFOCache(), simTime);
+        for (int i = 0; i < NUM_SAMPLES; i++) {
+            CacheSimulation sim = new CacheSimulation(M, N, new FIFOCache(), SIM_TIME);
             futureResults.add(executor.submit(sim::simulate));
         }
 
@@ -34,13 +37,21 @@ public class SimulationAverage {
             }
         });
 
-        double hitAverage = results.stream().mapToDouble(SimResults::getHitRatio).sum() / numSamples;
-        double missThroughputAverage = results.stream().mapToDouble(SimResults::getMissRateByMissThroughput).sum() / numSamples;
+        double hitAverage = results.stream().mapToDouble(SimResults::getHitRatio).sum() / NUM_SAMPLES;
+        double missThroughputAverage = results.stream().mapToDouble(SimResults::getMissRateByMissThroughput).sum() / NUM_SAMPLES;
+
+        double hitSD = calculateStandardDeviation(results, hitAverage, SimResults::getHitRatio);
+
+        double missThroughputSD = calculateStandardDeviation(
+                results,
+                missThroughputAverage,
+                SimResults::getMissRateByMissThroughput
+        );
 
         String outputFIFO = new StringBuilder()
-                .append("Virtual simulation time: " + simTime)
+                .append("Virtual simulation time: " + SIM_TIME)
                 .append(System.lineSeparator())
-                .append("Sample size: " + numSamples)
+                .append("Sample size: " + NUM_SAMPLES)
                 .append(System.lineSeparator())
                 .append("N: " + N)
                 .append(System.lineSeparator())
@@ -48,8 +59,23 @@ public class SimulationAverage {
                 .append(System.lineSeparator())
                 .append("Hit average: " + hitAverage)
                 .append(System.lineSeparator())
+                .append("Hit SD: " + hitSD)
+                .append(System.lineSeparator())
                 .append("Miss throughput average: " + missThroughputAverage)
+                .append(System.lineSeparator())
+                .append("Miss throughput SD: " + missThroughputSD)
                 .toString();
         System.out.println(outputFIFO);
+
+    }
+
+    private static double calculateStandardDeviation(List<SimResults> results, double missThroughputAverage, ToDoubleFunction<SimResults> resultFunc) {
+        final double squareMean = results.stream()
+                .mapToDouble(resultFunc)
+                .map(miss -> Math.pow(miss, 2))
+                .sum()
+                / NUM_SAMPLES;
+        final double variance = (squareMean - Math.pow(missThroughputAverage, 2)) * (NUM_SAMPLES / (NUM_SAMPLES - 1));
+        return Math.sqrt(variance);
     }
 }
